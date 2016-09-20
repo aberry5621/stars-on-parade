ff7b44d2a6ccf18c97626fa9775c1a6d209c44a5
rawr <aberry5621@mycom.marin.edu>
2016-09-19 19:27:47 -0700

    man up

--- HuzzaBuzza1/HuzzaBuzza1/huzza_buzza.cpp
+++ HuzzaBuzza1/HuzzaBuzza1/huzza_buzza.cpp
@@ -1,16 +1,37 @@
-// file: huzza_buzza.cpp
+// file: stars up and down.cpp
 // written by: alex
-// purpose: to create a buzz
-// forked by aberry5621 
+// purpose: display increasing numbers of stars and decreasing
 
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 int main() {
 
-	int foo = 123;
+	int input_num = 0;
 
-	cout << "huzza buzza ";
-	cout << foo;
+	cout << "star count" << endl;
+
+	cout << "enter a number: ";
+	cin >> input_num;
+
+	for (int i = 1; i <= input_num; i++)
+	{
+		for (int j = 1; j < i; j++)
+		{
+			cout << " * ";
+		}
+		cout << endl;
+	}
+
+	for (int i = input_num; i > 0; i--)
+	{
+		for (int j = 1; j < i; j++)
+		{
+			cout << " * ";
+		}
+		cout << endl;
+	}
+
 	return 0;
 }
