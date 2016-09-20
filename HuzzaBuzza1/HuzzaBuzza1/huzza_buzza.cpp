50723633dcc340913c83d83f9ec9d0e847435e98
Student <Student@SM130-D0022960>
2016-09-19 17:08:12 -0700

    I don't know.

--- HuzzaBuzza1/HuzzaBuzza1/huzza_buzza.cpp
+++ HuzzaBuzza1/HuzzaBuzza1/huzza_buzza.cpp
@@ -1,12 +1,16 @@
 // file: huzza_buzza.cpp
 // written by: alex
 // purpose: to create a buzz
-//
+// forked by aberry5621 
 
 #include<iostream>
 using namespace std;
 
 int main() {
-	cout << "huzza buzza";
+
+	int foo = 123;
+
+	cout << "huzza buzza ";
+	cout << foo;
 	return 0;
 }
